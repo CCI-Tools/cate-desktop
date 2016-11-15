e378f5fcf40d38d9063e23272cb1664c3259836d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-11-15 18:33:42 +0100

    adding a splitter

--- src/renderer/components.tsx
+++ src/renderer/components.tsx
@@ -1,38 +1,151 @@
 import * as React from 'react'
 
-export function HGLContainer(props) {
-    return createHGLElement('hgl-container', props);
+export class  HGLContainer extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-container">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLHeader(props) {
-    return createHGLElement('hgl-header', props);
+export class  HGLHeader extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-header">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLFooter(props) {
-    return createHGLElement('hgl-footer', props);
+export class  HGLFooter extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-footer">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLLeft(props) {
-    return createHGLElement('hgl-left', props);
+export class  HGLLeft extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-left">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLRight(props) {
-    return createHGLElement('hgl-right', props);
+export class  HGLRight extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-right">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLMidsection(props) {
-    return createHGLElement('hgl-midsection', props);
+
+export class  HGLCenter extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-center">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function HGLCenter(props) {
-    return createHGLElement('hgl-center', props);
+export class  HGLMidsection extends React.Component<any, any> {
+    render() {
+        return (
+            <div className="hgl-midsection">
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-function createHGLElement(className: string, props) {
-    return (
-        <div className={className}>
-            {props.children}
-        </div>
-    );
+
+interface HGLMidsection2Props {
+    left: HGLLeft;
+    center: HGLCenter;
+    right: HGLRight;
+    leftId?: string;
+    rightId?: string;
 }
 
+export class HGLMidsection2 extends React.Component<HGLMidsection2Props, any> {
+    constructor(props: HGLMidsection2Props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="hgl-midsection">
+                {this.props.left}
+                <HGLHorSplitter panelId={this.props.leftId || 'leftPanel'}/>
+                {this.props.center}
+                <HGLHorSplitter  panelId={this.props.leftId || 'rightPanel'}/>
+                {this.props.right}
+            </div>
+        );
+    }
+}
+
+
+interface HGLHorSplitterProps {
+    panelId: string;
+}
+
+// read: https://facebook.github.io/react/docs/refs-and-the-dom.html
+
+export class HGLHorSplitter extends React.Component<HGLHorSplitterProps, any> {
+
+    constructor(props: HGLHorSplitterProps) {
+        super(props);
+        this.onMouseDown.bind(this.onMouseDown, this);
+        this.onMouseUp.bind(this.onMouseUp, this);
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount: ', this);
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount: ', this);
+    }
+
+    onMouseDown(event: React.MouseEvent<HTMLInputElement>) {
+        console.log('onMouseDown: ', event, this);
+    }
+
+    onMouseUp(event: React.MouseEvent<HTMLInputElement>) {
+        console.log('onMouseUp: ', event, this);
+    }
+
+    onMouseMove(event: React.MouseEvent<HTMLInputElement>) {
+        console.log('onMouseMove: ', event, this);
+    }
+
+    onRef(splitter) {
+        console.log('onRef: ', splitter, this);
+    }
+
+    render() {
+        return (
+            <input
+                type="button"
+                className="hgl-hor-splitter"
+                onMouseDown={this.onMouseDown}
+                onMouseUp={this.onMouseUp}
+                onMouseMove={this.onMouseMove}
+                ref={this.onRef}/>
+        );
+    }
+}
