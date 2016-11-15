ee0edea35e001231eb0062543dba360cad322e5c
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-11-15 17:31:44 +0100

    added sample code for the Holy Grail Layout solved with Flexbox

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,22 +1,66 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Header, Footer, Section} from './components';
+import {HGLContainer, HGLHeader, HGLFooter, HGLMidsection, HGLLeft, HGLRight, HGLCenter} from './components';
 
 // TODO: we don't have a react-split-pane.d.ts yet, so we must use Node's CommonJS import
 // import * as SplitPane from 'react-split-pane';
 const SplitPane: any = require('react-split-pane');
 
+{/*<SplitPane defaultSize="40%" split="vertical">*/
+}
+{/*<div>Left</div>*/
+}
+{/*<div>Content</div>*/
+}
+{/*</SplitPane>*/
+}
 
 export function main() {
+    let imgContainerStyle = {
+        maxWidth: "100%",
+        maxHeight: "100%",
+        overflow: "scroll",
+    };
     ReactDOM.render(
-        <Section>
-            <Header>Header</Header>
-            <SplitPane defaultSize="40%" split="vertical">
-                <div>Left</div>
-                <div>Content</div>
-            </SplitPane>
-            <Footer>Footer</Footer>
-        </Section>,
+        <HGLContainer>
+            <HGLHeader>
+                <p>HEADER</p>
+            </HGLHeader>
+            <HGLMidsection>
+                <HGLLeft>
+                    <p>LEFT</p>
+                    <p>LEFT</p>
+                    <p>LEFT</p>
+                    <p>LEFT</p>
+                    <p>LEFT</p>
+                    <p>LEFT</p>
+                </HGLLeft>
+                <HGLCenter>
+                    {/*<div style={style1}>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                        {/*<p>CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER&nbsp;CENTER</p>*/}
+                    {/*</div>*/}
+                    <div style={imgContainerStyle}>
+                        <img src="resources/SST.png"/>
+                    </div>
+                </HGLCenter>
+                <HGLRight>
+                    <p>RIGHT</p>
+                    <p>RIGHT</p>
+                    <p>RIGHT</p>
+                    <p>RIGHT</p>
+                    <p>RIGHT</p>
+                    <p>RIGHT</p>
+                </HGLRight>
+            </HGLMidsection>
+            <HGLFooter>
+                <p>FOOTER</p>
+            </HGLFooter>
+        </HGLContainer>,
         document.getElementById('container')
     );
 }
--- src/renderer/components.tsx
+++ src/renderer/components.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react'
 
-export function Header(props) {
-    return (
-        <header>
-            {props.children}
-        </header>
-    );
+export function HGLContainer(props) {
+    return createHGLElement('hgl-container', props);
 }
 
-export function Footer(props) {
-    return (
-        <footer>
-            {props.children}
-        </footer>
-    );
+export function HGLHeader(props) {
+    return createHGLElement('hgl-header', props);
 }
 
-export function Section(props) {
+export function HGLFooter(props) {
+    return createHGLElement('hgl-footer', props);
+}
+
+export function HGLLeft(props) {
+    return createHGLElement('hgl-left', props);
+}
+
+export function HGLRight(props) {
+    return createHGLElement('hgl-right', props);
+}
+
+export function HGLMidsection(props) {
+    return createHGLElement('hgl-midsection', props);
+}
+
+export function HGLCenter(props) {
+    return createHGLElement('hgl-center', props);
+}
+
+function createHGLElement(className: string, props) {
     return (
-        <section className="root">
+        <div className={className}>
             {props.children}
-        </section>
+        </div>
     );
 }
 
