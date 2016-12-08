262e95507a6152b13229d679cce4cad91896ec30
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2016-12-08 17:01:28 +0100

    using lists instead of tree

--- src/renderer/components/WorkspacePanel.tsx
+++ src/renderer/components/WorkspacePanel.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {State, WorkspaceState} from "../state";
-import {Classes, Tree, ITreeNode, Tooltip} from "@blueprintjs/core";
+import {Tooltip, Tab, Tabs, TabList, TabPanel} from "@blueprintjs/core";
 import {ExpansionPanel} from "./ExpansionPanel";
 import {WorkspaceAPI} from "../webapi/apis/WorkspaceAPI";
 import * as actions from '../actions'
-
-export interface ITreeExampleState {
-    nodes: ITreeNode[];
-}
+import {ListBox} from "./ListBox";
 
 interface IWorkspacePanelProps {
     dispatch?: (action: {type: string, payload: any}) => void;
@@ -38,46 +35,10 @@ function mapStateToProps(state: State): IWorkspacePanelProps {
  *
  * @author Norman Fomferra
  */
-class WorkspacePanel extends React.Component<IWorkspacePanelProps, any> {
+class WorkspacePanel extends React.PureComponent<IWorkspacePanelProps, any> {
+
     constructor(props) {
         super();
-
-        const resourcesTooltip = "Workspace resources that result from workflow steps";
-        const stepsTooltip = "Workflow steps that generate resources";
-
-        const resourcesMoreMenu = <span className="pt-icon-standard pt-icon-edit"/>;
-        const operationsMoreMenu = <span className="pt-icon-standard pt-icon-more"/>;
-
-        this.state = {
-            nodes: [
-                {
-                    id: 'resources',
-                    hasCaret: true,
-                    isExpanded: true,
-                    iconName: "database",
-                    label: <Tooltip content={resourcesTooltip}>Resources</Tooltip>,
-                    childNodes: [
-                        {label: "Resource #1", secondaryLabel: resourcesMoreMenu,},
-                        {label: "Resource #2", secondaryLabel: resourcesMoreMenu,},
-                        {label: "Resource #3", secondaryLabel: resourcesMoreMenu,},
-                        {label: "Resource #4", secondaryLabel: resourcesMoreMenu,},
-                    ],
-                },
-                {
-                    id: 'steps',
-                    hasCaret: false,
-                    isExpanded: true,
-                    iconName: "function",
-                    label: <Tooltip content={stepsTooltip}>Workflow Steps</Tooltip>,
-                    childNodes: [
-                        {label: "Operation Step #1", secondaryLabel: operationsMoreMenu,},
-                        {label: "Operation Step #2", secondaryLabel: operationsMoreMenu,},
-                        {label: "Operation Step #3", secondaryLabel: operationsMoreMenu,},
-                        {label: "Operation Step #4", secondaryLabel: operationsMoreMenu,},
-                    ],
-                },
-            ],
-        } as any as ITreeExampleState;
     }
 
     private getWorkspaceAPI(): WorkspaceAPI {
@@ -87,14 +48,14 @@ class WorkspacePanel extends React.Component<IWorkspacePanelProps, any> {
     componentDidMount(): void {
         if (!this.props.workspace) {
             if (this.props.openLastWorkspace && this.props.lastWorkspacePath) {
-                this.openExistingWorkspace(this.props.lastWorkspacePath);
+                this.openWorkspace(this.props.lastWorkspacePath);
             } else {
-                this.createInitialWorkspace();
+                this.newWorkspace();
             }
         }
     }
 
-    private createInitialWorkspace() {
+    private newWorkspace() {
         // TODO: show in the UI that we are in the process of getting a new workspace
         this.getWorkspaceAPI().newWorkspace(null).then(workspace => {
             this.props.dispatch(actions.setCurrentWorkspace(workspace));
@@ -104,66 +65,61 @@ class WorkspacePanel extends React.Component<IWorkspacePanelProps, any> {
         });
     }
 
-    private openExistingWorkspace(workspacePath: string) {
+    private openWorkspace(workspacePath: string) {
         // TODO: show in the UI that we are in the process of opening a workspace
         this.getWorkspaceAPI().openWorkspace(workspacePath).then(workspace => {
             this.props.dispatch(actions.setCurrentWorkspace(workspace));
         }).catch(error => {
             // TODO: handle error
             console.error(error);
-            this.createInitialWorkspace();
+            this.newWorkspace();
         });
     }
 
-    //noinspection JSMethodCanBeStatic
-    /**
-     * Override @PureRender because nodes are not a primitive type and therefore aren't included in
-     * shallow prop comparison
-     */
-    public shouldComponentUpdate() {
-        return true;
-    }
-
-    private handleNodeClick = (nodeData: ITreeNode, _nodePath: number[], e: React.MouseEvent<HTMLElement>) => {
-        const originallySelected = nodeData.isSelected;
-        if (!e.shiftKey) {
-            this.forEachNode(this.state.nodes, (n) => n.isSelected = false);
-        }
-        nodeData.isSelected = originallySelected == null ? true : !originallySelected;
-        this.setState(this.state);
-    };
-
-    private handleNodeCollapse = (nodeData: ITreeNode) => {
-        nodeData.isExpanded = false;
-        this.setState(this.state);
-    };
-
-    private handleNodeExpand = (nodeData: ITreeNode) => {
-        nodeData.isExpanded = true;
-        this.setState(this.state);
-    };
-
-    private forEachNode(nodes: ITreeNode[], callback: (node: ITreeNode) => void) {
-        if (nodes == null) {
-            return;
-        }
-
-        for (const node of nodes) {
-            callback(node);
-            this.forEachNode(node.childNodes, callback);
-        }
-    }
-
     render() {
+        const resourcesTooltip = "Workspace resources that result from workflow steps";
+        const workflowTooltip = "Workflow steps that generate resources";
+
+        const resourcesMoreMenu = <span className="pt-icon-standard pt-icon-edit"/>;
+        const operationsMoreMenu = <span className="pt-icon-standard pt-icon-more"/>;
+
+        const resources = [
+            "Resource #1",
+            "Resource #2",
+            "Resource #3",
+            "Resource #4",
+        ];
+        const steps = [
+            "Operation Step #1",
+            "Operation Step #2",
+            "Operation Step #3",
+            "Operation Step #4",
+        ];
+
         return (
             <ExpansionPanel icon="pt-icon-folder-close" text="Workspace" isExpanded={true} defaultHeight={300}>
-                <Tree
-                    contents={this.state.nodes}
-                    onNodeClick={this.handleNodeClick}
-                    onNodeCollapse={this.handleNodeCollapse}
-                    onNodeExpand={this.handleNodeExpand}
-                    className={Classes.ELEVATION_1}
-                />
+                <Tabs>
+                    <TabList>
+                        <Tab>
+                            <Tooltip content={resourcesTooltip}>
+                                <span className="pt-icon-database" style={{marginRight: 4}}/>
+                            </Tooltip>
+                            <span>Resources</span>
+                        </Tab>
+                        <Tab>
+                            <Tooltip content={workflowTooltip}>
+                                <span className="pt-icon-database" style={{marginRight: 4}}/>
+                            </Tooltip>
+                            <span>Workflow</span>
+                        </Tab>
+                    </TabList>
+                    <TabPanel>
+                        <ListBox numItems={resources.length} renderItem={i => <span>{resources[i]}</span>}/>
+                    </TabPanel>
+                    <TabPanel>
+                        <ListBox numItems={steps.length} renderItem={i => <span>{steps[i]}</span>}/>
+                    </TabPanel>
+                </Tabs>
             </ExpansionPanel>
         );
     }
