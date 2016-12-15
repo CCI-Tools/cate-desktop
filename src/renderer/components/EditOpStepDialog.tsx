1ffd59ee94f5de893c912b69ba7221730d339ff9
Norman Fomferra <norman.fomferra@gmail.com>
2016-12-15 22:44:34 +0100

    Displaying op params

--- src/renderer/components/EditOpStepDialog.tsx
+++ src/renderer/components/EditOpStepDialog.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import {Dialog, Classes, Button, Tooltip} from "@blueprintjs/core";
+import {Dialog, Classes, Button, Tooltip, Checkbox} from "@blueprintjs/core";
 import {DialogState, OperationState} from "../state";
 
 interface IEditOpStepDialogProps {
     onClose: (actionId: string, dialogState: IEditOpStepDialogState) => void;
     operation: OperationState;
     isAddOpStepDialog: boolean;
+    parameterValues: any;
 }
 
 export interface IEditOpStepDialogState extends DialogState {
+    parameterValues: any;
 }
 
 export class EditOpStepDialog extends React.Component<IEditOpStepDialogProps, IEditOpStepDialogState> {
@@ -16,13 +18,14 @@ export class EditOpStepDialog extends React.Component<IEditOpStepDialogProps, IE
 
     constructor(props: IEditOpStepDialogProps) {
         super(props);
-        this.state = {isOpen: true};
+        this.state = {isOpen: true, parameterValues: props.parameterValues};
         this.dialogId = EditOpStepDialog.getDialogId(props.operation.name, props.isAddOpStepDialog);
         this.handleConfirm = this.handleConfirm.bind(this);
+        this.handleValidate = this.handleValidate.bind(this);
         this.handleCancel = this.handleCancel.bind(this);
     }
 
-    static getDialogId(operationName:string, isAddOpStepDialog: boolean) {
+    static getDialogId(operationName: string, isAddOpStepDialog: boolean) {
         if (!operationName) {
             return null;
         }
@@ -43,12 +46,17 @@ export class EditOpStepDialog extends React.Component<IEditOpStepDialogProps, IE
         this.close(null);
     }
 
+    //noinspection JSMethodCanBeStatic
+    private handleValidate() {
+        console.log('EditOpStepDialog: Validating...');
+    }
+
     render() {
 
-        const operationParamPanel = (<p>TODO...</p>); // TODO
+        const parameterPanel = this.renderParameterPanel();
 
         const dialogTitle = this.props.isAddOpStepDialog ? "Add Workflow Step" : "Change Workflow Step";
-        const tooltipText = this.props.isAddOpStepDialog? 'Add a new step to the workflow' : 'Change the step parameters.';
+        const tooltipText = this.props.isAddOpStepDialog ? 'Add a new step to the workflow' : 'Change the step parameters.';
 
         return (
             <Dialog
@@ -63,11 +71,12 @@ export class EditOpStepDialog extends React.Component<IEditOpStepDialogProps, IE
             >
                 <div className={Classes.DIALOG_BODY}>
                     <p><strong>{this.props.operation.name}</strong> parameters:</p>
-                    {operationParamPanel}
+                    {parameterPanel}
                 </div>
                 <div className={Classes.DIALOG_FOOTER}>
                     <div className={Classes.DIALOG_FOOTER_ACTIONS}>
                         <Button onClick={this.handleCancel}>Cancel</Button>
+                        <Button onClick={this.handleValidate}>Validate</Button>
                         <Tooltip content={tooltipText} inline>
                             <Button className="pt-intent-primary"
                                     onClick={this.handleConfirm}
@@ -77,5 +86,47 @@ export class EditOpStepDialog extends React.Component<IEditOpStepDialogProps, IE
                 </div>
             </Dialog>
         );
+    }
+
+    renderParameterPanel(): JSX.Element {
+        const operation = this.props.operation;
+        if (!operation.inputs || !operation.inputs.length) {
+            return null;
+        }
+
+        const createChild = (key: string, content: any) : JSX.Element => {
+            return (<div key={key}
+                         style={{display: 'flex' , padding: '0.2em'}}>{content}</div>);
+        };
+
+        const inputElems = operation.inputs.map(input => {
+            switch (input.dataType) {
+                case 'float':
+                case 'int':
+                case 'str': {
+                    const nameField = <span key="n" style={{flex: 'auto'}}>{input.name}</span>;
+                    const valueField = <input key="v" className="pt-input pt-intent-primary" type="text"/>;
+                    return createChild(input.name, [nameField, valueField]);
+                }
+                case 'bool': {
+                    return createChild(input.name, <Checkbox label={input.name}/>);
+                }
+                case 'Dataset': {
+                    const nameField = <span key="n" style={{flex: 'auto'}}>{input.name}</span>;
+                    const valueField = (
+                        <div key="v" className="pt-select pt-intent-primary">
+                            <select>
+                                <option key="0" value="_">Select dataset...</option>
+                                <option key="1" value="ds_1">ds_1</option>
+                                <option key="2" value="ds_2">ds_2</option>
+                                <option key="3" value="ds_3">ds_3</option>
+                                <option key="4" value="ds_4">ds_4</option>
+                            </select>
+                        </div>);
+                    return createChild(input.name, [nameField, valueField]);
+                }
+            }
+        });
+        return (<div>{inputElems}</div>);
     }
 }
