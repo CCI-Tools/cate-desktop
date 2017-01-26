a1b5389a376d77aee9d47565d448096e6b99fb67
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-01-26 20:32:49 +0100

    now opening directory chooser for new-, open-, save-workspace-as

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -11,7 +11,44 @@ import {State} from './state';
 import * as actions from './actions'
 import {stateReducer} from './reducers';
 
-//noinspection JSUnusedGlobalSymbols
+export interface FileFilter {
+    name: string;
+    extensions: string[];
+}
+
+type FileDialogProperty = 'openFile'|'openDirectory'|'multiSelections'|'createDirectory'|'showHiddenFiles';
+
+export interface SaveDialogOptions {
+    title?: string;
+    defaultPath?: string;
+    /**
+     * Custom label for the confirmation button, when left empty the default label will be used.
+     */
+    buttonLabel?: string;
+    filters?: FileFilter[];
+}
+
+export interface OpenDialogOptions extends SaveDialogOptions {
+    /**
+     * Contains which features the dialog should use.
+     */
+    properties?: FileDialogProperty[];
+    /**
+     * Normalize the keyboard access keys across platforms.
+     * Default is false. Enabling this assumes & is used in the button labels for the placement of the
+     * keyboard shortcut access key and labels will be converted so they work correctly on each platform,
+     * & characters are removed on macOS, converted to _ on Linux, and left untouched on Windows.
+     * For example, a button label of Vie&w will be converted to Vie_w on Linux and View on macOS and can
+     * be selected via Alt-W on Windows and Linux.
+     */
+    normalizeAccessKeys?: boolean;
+}
+
+function showOpenDialog(openDialogOptions: OpenDialogOptions) {
+    const workspacePaths = ipcRenderer.sendSync('show-open-dialog', true, openDialogOptions);
+    return workspacePaths && workspacePaths.length ? workspacePaths[0] : null;
+}
+
 export function main() {
 
     const middleware = applyMiddleware(
@@ -26,25 +63,51 @@ export function main() {
         connectWebAPIClient(store);
     });
 
-    ipcRenderer.on('new-workspace', (event) => {
-        store.dispatch(actions.newWorkspace())
+    ipcRenderer.on('new-workspace', () => {
+        const workspacePath = showOpenDialog({
+            title: "New Workspace",
+            buttonLabel: "Select",
+            properties: ['openDirectory', 'createDirectory'],
+        } as OpenDialogOptions);
+        if (workspacePath) {
+            store.dispatch(actions.newWorkspace(workspacePath));
+        }
     });
 
-    ipcRenderer.on('open-workspace', (event) => {
-        store.dispatch(actions.openWorkspace())
+    ipcRenderer.on('open-workspace', () => {
+        const workspacePath = showOpenDialog({
+            title: "Open Workspace",
+            buttonLabel: "Open",
+            properties: ['openDirectory'],
+        } as OpenDialogOptions);
+        if (workspacePath) {
+            store.dispatch(actions.openWorkspace(workspacePath));
+        }
     });
 
-    ipcRenderer.on('close-workspace', (event) => {
+    ipcRenderer.on('close-workspace', () => {
         store.dispatch(actions.closeWorkspace())
     });
 
-    ipcRenderer.on('save-workspace', (event) => {
-        store.dispatch(actions.saveWorkspace())
+    ipcRenderer.on('save-workspace', () => {
+        if (store.getState().data.workspace.isScratch) {
+            saveWorkspaceAs();
+        } else {
+            store.dispatch(actions.saveWorkspace())
+        }
     });
 
-    ipcRenderer.on('save-workspace-as', (event) => {
-        store.dispatch(actions.saveWorkspaceAs())
-    });
+    let saveWorkspaceAs = function () {
+        const workspacePath = showOpenDialog({
+            title: "Save Workspace As",
+            buttonLabel: "Select",
+            properties: ['openDirectory', 'createDirectory'],
+        } as OpenDialogOptions);
+        if (workspacePath) {
+            store.dispatch(actions.saveWorkspaceAs(workspacePath))
+        }
+    };
+    ipcRenderer.on('save-workspace-as', saveWorkspaceAs);
 }
 
 function connectWebAPIClient(store: Store<State>) {
