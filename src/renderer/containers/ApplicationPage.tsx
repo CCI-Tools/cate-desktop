1cea13c6b84e1da1a65c38efbf71e5d9c2d1a9b0
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-04-07 16:38:21 +0200

    split ApplicationPage into 3 different component to reduce rendering

--- src/renderer/containers/ApplicationPage.tsx
+++ src/renderer/containers/ApplicationPage.tsx
@@ -33,29 +33,174 @@ interface IDispatch {
     dispatch: Dispatch<State>;
 }
 
-interface IApplicationPageProps {
+//noinspection JSUnusedLocalSymbols
+export default class ApplicationPage extends React.PureComponent<null, null> {
+
+    render() {
+
+        const centerStyle = {
+            flex: "auto",
+            maxHeight: "100%",
+        };
+
+        return (
+            <div style={{display: "flex", flexFlow: "column nowrap", width: "100%", height: "100%", }}>
+                <div style={{flex: "auto", padding: 0, display:"flex", flexFlow: "row nowrap"}}>
+                    <LeftPanel/>
+                    <div style={centerStyle}>
+                        <ViewManagerPanel/>
+                    </div>
+                    <RightPanel/>
+                </div>
+                <StatusBar/>
+                <PreferencesDialog/>
+                <div id="creditContainer"
+                     style={{minWidth: "10em", minHeight: "4em", position: "relative", overflow: "auto", display: "none"}}/>
+
+            </div>
+        );
+    }
+}
+
+interface ILeftPanelProps {
     panelContainerUndockedMode: boolean;
     leftPanelContainerLayout: PanelContainerLayout;
-    rightPanelContainerLayout: PanelContainerLayout;
     selectedLeftTopPanelId?: string;
     selectedLeftBottomPanelId?: string;
+}
+
+function mapStateToPropsLeft(state: State): ILeftPanelProps {
+    return {
+        panelContainerUndockedMode: selectors.panelContainerUndockedModeSelector(state),
+        leftPanelContainerLayout: selectors.leftPanelContainerLayoutSelector(state),
+        selectedLeftTopPanelId: selectors.selectedLeftTopPanelIdSelector(state),
+        selectedLeftBottomPanelId: selectors.selectedLeftBottomPanelIdSelector(state),
+    };
+}
+
+//noinspection JSUnusedLocalSymbols
+class LeftPanelComponent extends React.PureComponent<ILeftPanelProps & IDispatch, null> {
+
+    constructor(props: ILeftPanelProps & IDispatch) {
+        super(props);
+        this.onLeftPanelContainerLayoutChange = this.onLeftPanelContainerLayoutChange.bind(this);
+        this.onRightPanelContainerLayoutChange = this.onRightPanelContainerLayoutChange.bind(this);
+        this.onSelectedLeftTopPanelChange = this.onSelectedLeftTopPanelChange.bind(this);
+        this.onSelectedLeftBottomPanelChange = this.onSelectedLeftBottomPanelChange.bind(this);
+    }
+
+    onLeftPanelContainerLayoutChange(layout: PanelContainerLayout) {
+        this.props.dispatch(actions.setLeftPanelContainerLayout(layout));
+    }
+
+    onRightPanelContainerLayoutChange(layout: PanelContainerLayout) {
+        this.props.dispatch(actions.setRightPanelContainerLayout(layout));
+    }
+
+    onSelectedLeftTopPanelChange(id: string|null) {
+        this.props.dispatch(actions.setSelectedLeftTopPanelId(id));
+    }
+
+    onSelectedLeftBottomPanelChange(id: string|null) {
+        this.props.dispatch(actions.setSelectedLeftBottomPanelId(id));
+    }
+
+    render() {
+
+        return (
+            <PanelContainer position="left"
+                            undockedMode={this.props.panelContainerUndockedMode}
+                            layout={this.props.leftPanelContainerLayout}
+                            onLayoutChange={this.onLeftPanelContainerLayoutChange}
+                            selectedTopPanelId={this.props.selectedLeftTopPanelId}
+                            selectedBottomPanelId={this.props.selectedLeftBottomPanelId}
+                            onSelectedTopPanelChange={this.onSelectedLeftTopPanelChange}
+                            onSelectedBottomPanelChange={this.onSelectedLeftBottomPanelChange}
+            >
+                <Panel id="dataSources" position="top" iconName="pt-icon-database" title="Data Sources"
+                       body={<DataSourcesPanel/>}/>
+                <Panel id="operations" position="top" iconName="pt-icon-function" title="Operations"
+                       body={<OperationsPanel/>}/>
+                <Panel id="workspace" position="bottom" iconName="pt-icon-folder-close" title="Workspace"
+                       body={<WorkspacePanel/>}/>
+            </PanelContainer>
+        );
+    }
+}
+const LeftPanel = connect(mapStateToPropsLeft)(LeftPanelComponent);
+
+interface IRightPanelProps {
+    panelContainerUndockedMode: boolean;
+    rightPanelContainerLayout: PanelContainerLayout;
     selectedRightTopPanelId?: string;
     selectedRightBottomPanelId?: string;
+}
 
+function mapStateToPropsRight(state: State): IRightPanelProps {
+    return {
+        panelContainerUndockedMode: selectors.panelContainerUndockedModeSelector(state),
+        rightPanelContainerLayout: selectors.rightPanelContainerLayoutSelector(state),
+        selectedRightTopPanelId: selectors.selectedRightTopPanelIdSelector(state),
+        selectedRightBottomPanelId: selectors.selectedRightBottomPanelIdSelector(state),
+    };
+}
+
+//noinspection JSUnusedLocalSymbols
+class RightPanelComponent extends React.PureComponent<IRightPanelProps & IDispatch, null> {
+
+    constructor(props: IRightPanelProps & IDispatch) {
+        super(props);
+        this.onRightPanelContainerLayoutChange = this.onRightPanelContainerLayoutChange.bind(this);
+        this.onSelectedRightTopPanelChange = this.onSelectedRightTopPanelChange.bind(this);
+        this.onSelectedRightBottomPanelChange = this.onSelectedRightBottomPanelChange.bind(this);
+    }
+
+    onRightPanelContainerLayoutChange(layout: PanelContainerLayout) {
+        this.props.dispatch(actions.setRightPanelContainerLayout(layout));
+    }
+
+    onSelectedRightTopPanelChange(id: string|null) {
+        this.props.dispatch(actions.setSelectedRightTopPanelId(id));
+    }
+
+    onSelectedRightBottomPanelChange(id: string|null) {
+        this.props.dispatch(actions.setSelectedRightBottomPanelId(id));
+    }
+
+    render() {
+        return (
+            <PanelContainer position="right"
+                            undockedMode={this.props.panelContainerUndockedMode}
+                            layout={this.props.rightPanelContainerLayout}
+                            onLayoutChange={this.onRightPanelContainerLayoutChange}
+                            selectedTopPanelId={this.props.selectedRightTopPanelId}
+                            selectedBottomPanelId={this.props.selectedRightBottomPanelId}
+                            onSelectedTopPanelChange={this.onSelectedRightTopPanelChange}
+                            onSelectedBottomPanelChange={this.onSelectedRightBottomPanelChange}
+            >
+                <Panel id="variables" position="top" iconName="pt-icon-variable" title="Variables"
+                       body={<VariablePanel/>}/>
+                <Panel id="analysis" position="top" iconName="pt-icon-scatter-plot" title="Analysis"
+                       body={null}/>
+                <Panel id="view" position="bottom" iconName="pt-icon-eye-open" title="View"
+                       body={<ViewPanel/>}/>
+                <Panel id="layers" position="bottom" iconName="pt-icon-layers" title="Layers"
+                       body={<LayersPanel/>}/>
+                <Panel id="tasks" position="bottom" iconName="pt-icon-play" title="Tasks" body={<TaskPanel/>}/>
+            </PanelContainer>
+        );
+    }
+}
+const RightPanel = connect(mapStateToPropsRight)(RightPanelComponent);
+
+interface IViewManagerPanelProps {
     viewLayout: ViewLayoutState;
     views: ViewState<any>[];
     activeView: ViewState<any> | null;
 }
 
-function mapStateToProps(state: State): IApplicationPageProps {
+function mapStateToPropsView(state: State): IViewManagerPanelProps {
     return {
-        panelContainerUndockedMode: selectors.panelContainerUndockedModeSelector(state),
-        leftPanelContainerLayout: selectors.leftPanelContainerLayoutSelector(state),
-        rightPanelContainerLayout: selectors.rightPanelContainerLayoutSelector(state),
-        selectedLeftTopPanelId: selectors.selectedLeftTopPanelIdSelector(state),
-        selectedLeftBottomPanelId: selectors.selectedLeftBottomPanelIdSelector(state),
-        selectedRightTopPanelId: selectors.selectedRightTopPanelIdSelector(state),
-        selectedRightBottomPanelId: selectors.selectedRightBottomPanelIdSelector(state),
         viewLayout: selectors.viewLayoutSelector(state),
         views: selectors.viewsSelector(state),
         activeView: selectors.activeViewSelector(state),
@@ -63,17 +208,10 @@ function mapStateToProps(state: State): IApplicationPageProps {
 }
 
 //noinspection JSUnusedLocalSymbols
-class ApplicationPage extends React.PureComponent<IApplicationPageProps & IDispatch, null> {
+class ViewManagerPanelComponent extends React.PureComponent<IViewManagerPanelProps & IDispatch, null> {
 
-    constructor(props: IApplicationPageProps & IDispatch) {
+    constructor(props: IViewManagerPanelProps & IDispatch) {
         super(props);
-        this.onLeftPanelContainerLayoutChange = this.onLeftPanelContainerLayoutChange.bind(this);
-        this.onRightPanelContainerLayoutChange = this.onRightPanelContainerLayoutChange.bind(this);
-        this.onSelectedLeftTopPanelChange = this.onSelectedLeftTopPanelChange.bind(this);
-        this.onSelectedLeftBottomPanelChange = this.onSelectedLeftBottomPanelChange.bind(this);
-        this.onSelectedRightTopPanelChange = this.onSelectedRightTopPanelChange.bind(this);
-        this.onSelectedRightBottomPanelChange = this.onSelectedRightBottomPanelChange.bind(this);
-
         this.onSelectView = this.onSelectView.bind(this);
         this.onCloseView = this.onCloseView.bind(this);
         this.onCloseAllViews = this.onCloseAllViews.bind(this);
@@ -101,102 +239,24 @@ class ApplicationPage extends React.PureComponent<IApplicationPageProps & IDispa
         this.props.dispatch(actions.changeViewSplitPos(viewPath, delta));
     }
 
-    onLeftPanelContainerLayoutChange(layout: PanelContainerLayout) {
-        this.props.dispatch(actions.setLeftPanelContainerLayout(layout));
-    }
-
-    onRightPanelContainerLayoutChange(layout: PanelContainerLayout) {
-        this.props.dispatch(actions.setRightPanelContainerLayout(layout));
-    }
-
-    onSelectedLeftTopPanelChange(id: string|null) {
-        this.props.dispatch(actions.setSelectedLeftTopPanelId(id));
-    }
-
-    onSelectedLeftBottomPanelChange(id: string|null) {
-        this.props.dispatch(actions.setSelectedLeftBottomPanelId(id));
-    }
-
-    onSelectedRightTopPanelChange(id: string|null) {
-        this.props.dispatch(actions.setSelectedRightTopPanelId(id));
-    }
-
-    onSelectedRightBottomPanelChange(id: string|null) {
-        this.props.dispatch(actions.setSelectedRightBottomPanelId(id));
-    }
-
     render() {
-
-        const centerStyle = {
-            flex: "auto",
-            maxHeight: "100%",
-        };
-
         return (
-            <div style={{display: "flex", flexFlow: "column nowrap", width: "100%", height: "100%", }}>
-                <div style={{flex: "auto", padding: 0, display:"flex", flexFlow: "row nowrap"}}>
-                    <PanelContainer position="left"
-                                    undockedMode={this.props.panelContainerUndockedMode}
-                                    layout={this.props.leftPanelContainerLayout}
-                                    onLayoutChange={this.onLeftPanelContainerLayoutChange}
-                                    selectedTopPanelId={this.props.selectedLeftTopPanelId}
-                                    selectedBottomPanelId={this.props.selectedLeftBottomPanelId}
-                                    onSelectedTopPanelChange={this.onSelectedLeftTopPanelChange}
-                                    onSelectedBottomPanelChange={this.onSelectedLeftBottomPanelChange}
-                    >
-                        <Panel id="dataSources" position="top" iconName="pt-icon-database" title="Data Sources"
-                               body={<DataSourcesPanel/>}/>
-                        <Panel id="operations" position="top" iconName="pt-icon-function" title="Operations"
-                               body={<OperationsPanel/>}/>
-                        <Panel id="workspace" position="bottom" iconName="pt-icon-folder-close" title="Workspace"
-                               body={<WorkspacePanel/>}/>
-                    </PanelContainer>
-                    <div style={centerStyle}>
-                        <ViewManager viewRenderMap={VIEW_TYPE_RENDERERS}
-                                     viewLayout={this.props.viewLayout}
-                                     views={this.props.views}
-                                     activeView={this.props.activeView}
-                                     noViewsDescription="You can create a new view in the VIEW panel."
-                                     noViewsVisual="pt-icon-eye-open"
-                                     onSelectView={this.onSelectView}
-                                     onCloseView={this.onCloseView}
-                                     onCloseAllViews={this.onCloseAllViews}
-                                     onChangeViewSplitPos={this.onChangeViewSplitPos}
-                                     onSplitViewPanel={this.onSplitViewPanel}
-                        />
-                    </div>
-                    <PanelContainer position="right"
-                                    undockedMode={this.props.panelContainerUndockedMode}
-                                    layout={this.props.rightPanelContainerLayout}
-                                    onLayoutChange={this.onRightPanelContainerLayoutChange}
-                                    selectedTopPanelId={this.props.selectedRightTopPanelId}
-                                    selectedBottomPanelId={this.props.selectedRightBottomPanelId}
-                                    onSelectedTopPanelChange={this.onSelectedRightTopPanelChange}
-                                    onSelectedBottomPanelChange={this.onSelectedRightBottomPanelChange}
-                    >
-                        <Panel id="variables" position="top" iconName="pt-icon-variable" title="Variables"
-                               body={<VariablePanel/>}/>
-                        <Panel id="analysis" position="top" iconName="pt-icon-scatter-plot" title="Analysis"
-                               body={null}/>
-                        <Panel id="view" position="bottom" iconName="pt-icon-eye-open" title="View"
-                               body={<ViewPanel/>}/>
-                        <Panel id="layers" position="bottom" iconName="pt-icon-layers" title="Layers"
-                               body={<LayersPanel/>}/>
-                        <Panel id="tasks" position="bottom" iconName="pt-icon-play" title="Tasks" body={<TaskPanel/>}/>
-                    </PanelContainer>
-                </div>
-                <StatusBar/>
-                <PreferencesDialog/>
-                <div id="creditContainer"
-                     style={{minWidth: "10em", minHeight: "4em", position: "relative", overflow: "auto", display: "none"}}/>
-
-            </div>
+            <ViewManager viewRenderMap={VIEW_TYPE_RENDERERS}
+                         viewLayout={this.props.viewLayout}
+                         views={this.props.views}
+                         activeView={this.props.activeView}
+                         noViewsDescription="You can create a new view in the VIEW panel."
+                         noViewsVisual="pt-icon-eye-open"
+                         onSelectView={this.onSelectView}
+                         onCloseView={this.onCloseView}
+                         onCloseAllViews={this.onCloseAllViews}
+                         onChangeViewSplitPos={this.onChangeViewSplitPos}
+                         onSplitViewPanel={this.onSplitViewPanel}
+            />
         );
     }
 }
-
-export default connect(mapStateToProps)(ApplicationPage);
-
+const ViewManagerPanel = connect(mapStateToPropsView)(ViewManagerPanelComponent);
 
 
 
