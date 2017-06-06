cb27b08192992f3a0c6295fc9c45e0d505ad8b2d
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-06-06 18:39:42 +0200

    convert to valid TS

--- src/renderer/components/matplotlib/mpl.ts
+++ src/renderer/components/matplotlib/mpl.ts
@@ -1,19 +1,11 @@
 /* Put everything inside the global mpl namespace */
-window.mpl = {};
-
+const mpl = {} as any;
+export default mpl;
+let $;
 
 mpl.get_websocket_type = function() {
-    if (typeof(WebSocket) !== 'undefined') {
-        return WebSocket;
-    } else if (typeof(MozWebSocket) !== 'undefined') {
-        return MozWebSocket;
-    } else {
-        alert('Your browser does not have WebSocket support.' +
-            'Please try Chrome, Safari or Firefox â‰¥ 6. ' +
-            'Firefox 4 and 5 are also supported but you ' +
-            'have to enable WebSockets in about:config.');
-    };
-}
+    return WebSocket;
+};
 
 mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
     this.id = figure_id;
@@ -23,7 +15,7 @@ mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
     this.supports_binary = (this.ws.binaryType != undefined);
 
     if (!this.supports_binary) {
-        var warnings = document.getElementById("mpl-warnings");
+        const warnings = document.getElementById("mpl-warnings");
         if (warnings) {
             warnings.style.display = 'block';
             warnings.textContent = (
@@ -44,7 +36,7 @@ mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
     this.image_mode = 'full';
 
     this.root = $('<div/>');
-    this._root_extra_style(this.root)
+    this._root_extra_style(this.root);
     this.root.attr('style', 'display: inline-block');
 
     $(parent_element).append(this.root);
@@ -53,7 +45,7 @@ mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
     this._init_canvas(this);
     this._init_toolbar(this);
 
-    var fig = this;
+    const fig = this;
 
     this.waiting = false;
 
@@ -64,7 +56,7 @@ mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
             fig.send_message("set_dpi_ratio", {'dpi_ratio': mpl.ratio});
         }
         fig.send_message("refresh", {});
-    }
+    };
 
     this.imageObj.onload = function() {
         if (fig.image_mode == 'full') {
@@ -77,41 +69,41 @@ mpl.figure = function(figure_id, websocket, ondownload, parent_element) {
     };
 
     this.imageObj.onunload = function() {
-        this.ws.close();
-    }
+        fig.ws.close();
+    };
 
     this.ws.onmessage = this._make_on_message_function(this);
 
     this.ondownload = ondownload;
-}
+};
 
 mpl.figure.prototype._init_header = function() {
-    var titlebar = $(
+    const titlebar = $(
         '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ' +
         'ui-helper-clearfix"/>');
-    var titletext = $(
+    const titletext = $(
         '<div class="ui-dialog-title" style="width: 100%; ' +
         'text-align: center; padding: 3px;"/>');
-    titlebar.append(titletext)
+    titlebar.append(titletext);
     this.root.append(titlebar);
     this.header = titletext[0];
-}
+};
 
 
 
 mpl.figure.prototype._canvas_extra_style = function(canvas_div) {
 
-}
+};
 
 
 mpl.figure.prototype._root_extra_style = function(canvas_div) {
 
-}
+};
 
 mpl.figure.prototype._init_canvas = function() {
-    var fig = this;
+    const fig = this;
 
-    var canvas_div = $('<div/>');
+    const canvas_div = $('<div/>');
 
     canvas_div.attr('style', 'position: relative; clear: both; outline: 0');
 
@@ -121,18 +113,18 @@ mpl.figure.prototype._init_canvas = function() {
 
     canvas_div.keydown('key_press', canvas_keyboard_event);
     canvas_div.keyup('key_release', canvas_keyboard_event);
-    this.canvas_div = canvas_div
-    this._canvas_extra_style(canvas_div)
+    this.canvas_div = canvas_div;
+    this._canvas_extra_style(canvas_div);
     this.root.append(canvas_div);
 
-    var canvas = $('<canvas/>');
+    const canvas = $('<canvas/>');
     canvas.addClass('mpl-canvas');
-    canvas.attr('style', "left: 0; top: 0; z-index: 0; outline: 0")
+    canvas.attr('style', "left: 0; top: 0; z-index: 0; outline: 0");
 
     this.canvas = canvas[0];
     this.context = canvas[0].getContext("2d");
 
-    var backingStore = this.context.backingStorePixelRatio ||
+    const backingStore = this.context.backingStorePixelRatio ||
         this.context.webkitBackingStorePixelRatio ||
         this.context.mozBackingStorePixelRatio ||
         this.context.msBackingStorePixelRatio ||
@@ -141,10 +133,10 @@ mpl.figure.prototype._init_canvas = function() {
 
     mpl.ratio = (window.devicePixelRatio || 1) / backingStore;
 
-    var rubberband = $('<canvas/>');
-    rubberband.attr('style', "position: absolute; left: 0; top: 0; z-index: 1;")
+    const rubberband = $('<canvas/>');
+    rubberband.attr('style', "position: absolute; left: 0; top: 0; z-index: 1;");
 
-    var pass_mouse_events = true;
+    let pass_mouse_events = true;
 
     canvas_div.resizable({
         start: function(event, ui) {
@@ -174,7 +166,7 @@ mpl.figure.prototype._init_canvas = function() {
 
     canvas_div.on("wheel", function (event) {
         event = event.originalEvent;
-        event['data'] = 'scroll'
+        event['data'] = 'scroll';
         if (event.deltaY < 0) {
             event.step = 1;
         } else {
@@ -194,8 +186,8 @@ mpl.figure.prototype._init_canvas = function() {
     this._resize_canvas = function(width, height) {
         // Keep the size of the canvas, canvas container, and rubber band
         // canvas in synch.
-        canvas_div.css('width', width)
-        canvas_div.css('height', height)
+        canvas_div.css('width', width);
+        canvas_div.css('height', height);
 
         canvas.attr('width', width * mpl.ratio);
         canvas.attr('height', height * mpl.ratio);
@@ -203,7 +195,7 @@ mpl.figure.prototype._init_canvas = function() {
 
         rubberband.attr('width', width);
         rubberband.attr('height', height);
-    }
+    };
 
     // Set the figure to an initial 600x600px, this will subsequently be updated
     // upon first draw.
@@ -220,12 +212,12 @@ mpl.figure.prototype._init_canvas = function() {
     }
 
     window.setTimeout(set_focus, 100);
-}
+};
 
 mpl.figure.prototype._init_toolbar = function() {
-    var fig = this;
+    const fig = this;
 
-    var nav_element = $('<div/>')
+    const nav_element = $('<div/>');
     nav_element.attr('style', 'width: 100%');
     this.root.append(nav_element);
 
@@ -237,17 +229,17 @@ mpl.figure.prototype._init_toolbar = function() {
         return fig.toolbar_button_onmouseover(event['data']);
     }
 
-    for(var toolbar_ind in mpl.toolbar_items) {
-        var name = mpl.toolbar_items[toolbar_ind][0];
-        var tooltip = mpl.toolbar_items[toolbar_ind][1];
-        var image = mpl.toolbar_items[toolbar_ind][2];
-        var method_name = mpl.toolbar_items[toolbar_ind][3];
+    for(let toolbar_ind in mpl.toolbar_items) {
+        let name = mpl.toolbar_items[toolbar_ind][0];
+        const tooltip = mpl.toolbar_items[toolbar_ind][1];
+        const image = mpl.toolbar_items[toolbar_ind][2];
+        const method_name = mpl.toolbar_items[toolbar_ind][3];
 
         if (!name) {
             // put a spacer in here.
             continue;
         }
-        var button = $('<button/>');
+        const button = $('<button/>');
         button.addClass('ui-button ui-widget ui-state-default ui-corner-all ' +
             'ui-button-icon-only');
         button.attr('role', 'button');
@@ -255,12 +247,12 @@ mpl.figure.prototype._init_toolbar = function() {
         button.click(method_name, toolbar_event);
         button.mouseover(tooltip, toolbar_mouse_event);
 
-        var icon_img = $('<span/>');
+        const icon_img = $('<span/>');
         icon_img.addClass('ui-button-icon-primary ui-icon');
         icon_img.addClass(image);
         icon_img.addClass('ui-corner-all');
 
-        var tooltip_span = $('<span/>');
+        const tooltip_span = $('<span/>');
         tooltip_span.addClass('ui-button-text');
         tooltip_span.html(tooltip);
 
@@ -270,17 +262,17 @@ mpl.figure.prototype._init_toolbar = function() {
         nav_element.append(button);
     }
 
-    var fmt_picker_span = $('<span/>');
+    const fmt_picker_span = $('<span/>');
 
-    var fmt_picker = $('<select/>');
+    const fmt_picker = $('<select/>');
     fmt_picker.addClass('mpl-toolbar-option ui-widget ui-widget-content');
     fmt_picker_span.append(fmt_picker);
     nav_element.append(fmt_picker_span);
     this.format_dropdown = fmt_picker[0];
 
-    for (var ind in mpl.extensions) {
-        var fmt = mpl.extensions[ind];
-        var option = $(
+    for (let ind in mpl.extensions) {
+        const fmt = mpl.extensions[ind];
+        const option = $(
             '<option/>', {selected: fmt === mpl.default_extension}).html(fmt);
         fmt_picker.append(option)
     }
@@ -291,73 +283,73 @@ mpl.figure.prototype._init_toolbar = function() {
         function() { $(this).removeClass("ui-state-hover");}
     );
 
-    var status_bar = $('<span class="mpl-message"/>');
+    const status_bar = $('<span class="mpl-message"/>');
     nav_element.append(status_bar);
     this.message = status_bar[0];
-}
+};
 
 mpl.figure.prototype.request_resize = function(x_pixels, y_pixels) {
     // Request matplotlib to resize the figure. Matplotlib will then trigger a resize in the client,
     // which will in turn request a refresh of the image.
     this.send_message('resize', {'width': x_pixels, 'height': y_pixels});
-}
+};
 
 mpl.figure.prototype.send_message = function(type, properties) {
     properties['type'] = type;
     properties['figure_id'] = this.id;
     this.ws.send(JSON.stringify(properties));
-}
+};
 
 mpl.figure.prototype.send_draw_message = function() {
     if (!this.waiting) {
         this.waiting = true;
         this.ws.send(JSON.stringify({type: "draw", figure_id: this.id}));
     }
-}
+};
 
 
 mpl.figure.prototype.handle_save = function(fig, msg) {
-    var format_dropdown = fig.format_dropdown;
-    var format = format_dropdown.options[format_dropdown.selectedIndex].value;
+    const format_dropdown = fig.format_dropdown;
+    const format = format_dropdown.options[format_dropdown.selectedIndex].value;
     fig.ondownload(fig, format);
-}
+};
 
 
 mpl.figure.prototype.handle_resize = function(fig, msg) {
-    var size = msg['size'];
+    const size = msg['size'];
     if (size[0] != fig.canvas.width || size[1] != fig.canvas.height) {
         fig._resize_canvas(size[0], size[1]);
         fig.send_message("refresh", {});
-    };
-}
+    }
+};
 
 mpl.figure.prototype.handle_rubberband = function(fig, msg) {
-    var x0 = msg['x0'] / mpl.ratio;
-    var y0 = (fig.canvas.height - msg['y0']) / mpl.ratio;
-    var x1 = msg['x1'] / mpl.ratio;
-    var y1 = (fig.canvas.height - msg['y1']) / mpl.ratio;
+    let x0 = msg['x0'] / mpl.ratio;
+    let y0 = (fig.canvas.height - msg['y0']) / mpl.ratio;
+    let x1 = msg['x1'] / mpl.ratio;
+    let y1 = (fig.canvas.height - msg['y1']) / mpl.ratio;
     x0 = Math.floor(x0) + 0.5;
     y0 = Math.floor(y0) + 0.5;
     x1 = Math.floor(x1) + 0.5;
     y1 = Math.floor(y1) + 0.5;
-    var min_x = Math.min(x0, x1);
-    var min_y = Math.min(y0, y1);
-    var width = Math.abs(x1 - x0);
-    var height = Math.abs(y1 - y0);
+    const min_x = Math.min(x0, x1);
+    const min_y = Math.min(y0, y1);
+    const width = Math.abs(x1 - x0);
+    const height = Math.abs(y1 - y0);
 
     fig.rubberband_context.clearRect(
         0, 0, fig.canvas.width, fig.canvas.height);
 
     fig.rubberband_context.strokeRect(min_x, min_y, width, height);
-}
+};
 
 mpl.figure.prototype.handle_figure_label = function(fig, msg) {
     // Updates the figure title.
     fig.header.textContent = msg['label'];
-}
+};
 
 mpl.figure.prototype.handle_cursor = function(fig, msg) {
-    var cursor = msg['cursor'];
+    let cursor = msg['cursor'];
     switch(cursor)
     {
         case 0:
@@ -374,25 +366,25 @@ mpl.figure.prototype.handle_cursor = function(fig, msg) {
             break;
     }
     fig.rubberband_canvas.style.cursor = cursor;
-}
+};
 
 mpl.figure.prototype.handle_message = function(fig, msg) {
     fig.message.textContent = msg['message'];
-}
+};
 
 mpl.figure.prototype.handle_draw = function(fig, msg) {
     // Request the server to send over a new figure.
     fig.send_draw_message();
-}
+};
 
 mpl.figure.prototype.handle_image_mode = function(fig, msg) {
     fig.image_mode = msg['mode'];
-}
+};
 
 mpl.figure.prototype.updated_canvas_event = function() {
     // Called whenever the canvas gets updated.
     this.send_message("ack", {});
-}
+};
 
 // A function to construct a web socket function for onmessage handling.
 // Called in the figure constructor.
@@ -407,11 +399,11 @@ mpl.figure.prototype._make_on_message_function = function(fig) {
 
             /* Free the memory for the previous frames */
             if (fig.imageObj.src) {
-                (window.URL || window.webkitURL).revokeObjectURL(
+                (window.URL || window['webkitURL']).revokeObjectURL(
                     fig.imageObj.src);
             }
 
-            fig.imageObj.src = (window.URL || window.webkitURL).createObjectURL(
+            fig.imageObj.src = (window.URL || window['webkitURL']).createObjectURL(
                 evt.data);
             fig.updated_canvas_event();
             fig.waiting = false;
@@ -424,13 +416,14 @@ mpl.figure.prototype._make_on_message_function = function(fig) {
             return;
         }
 
-        var msg = JSON.parse(evt.data);
-        var msg_type = msg['type'];
+        const msg = JSON.parse(evt.data);
+        const msg_type = msg['type'];
 
         // Call the  "handle_{type}" callback, which takes
         // the figure and JSON message as its only arguments.
+        let callback;
         try {
-            var callback = fig["handle_" + msg_type];
+            callback = fig["handle_" + msg_type];
         } catch (e) {
             console.log("No handler for the '" + msg_type + "' message type: ", msg);
             return;
@@ -445,12 +438,12 @@ mpl.figure.prototype._make_on_message_function = function(fig) {
             }
         }
     };
-}
+};
 
 // from http://stackoverflow.com/questions/1114465/getting-mouse-location-in-canvas
 mpl.findpos = function(e) {
     //this section is from http://www.quirksmode.org/js/events_properties.html
-    var targ;
+    let targ;
     if (!e)
         e = window.event;
     if (e.target)
@@ -463,8 +456,8 @@ mpl.findpos = function(e) {
     // jQuery normalizes the pageX and pageY
     // pageX,Y are the mouse positions relative to the document
     // offset() returns the position of the element relative to the document
-    var x = e.pageX - $(targ).offset().left;
-    var y = e.pageY - $(targ).offset().top;
+    const x = e.pageX - $(targ).offset().left;
+    const y = e.pageY - $(targ).offset().top;
 
     return {"x": x, "y": y};
 };
@@ -477,13 +470,13 @@ mpl.findpos = function(e) {
 function simpleKeys (original) {
     return Object.keys(original).reduce(function (obj, key) {
         if (typeof original[key] !== 'object')
-            obj[key] = original[key]
+            obj[key] = original[key];
         return obj;
     }, {});
 }
 
 mpl.figure.prototype.mouse_event = function(event, name) {
-    var canvas_pos = mpl.findpos(event)
+    const canvas_pos = mpl.findpos(event);
 
     if (name === 'button_press')
     {
@@ -491,8 +484,8 @@ mpl.figure.prototype.mouse_event = function(event, name) {
         this.canvas_div.focus();
     }
 
-    var x = canvas_pos.x * mpl.ratio;
-    var y = canvas_pos.y * mpl.ratio;
+    const x = canvas_pos.x * mpl.ratio;
+    const y = canvas_pos.y * mpl.ratio;
 
     this.send_message(name, {x: x, y: y, button: event.button,
         step: event.step,
@@ -504,11 +497,11 @@ mpl.figure.prototype.mouse_event = function(event, name) {
      * 'cursor' event from matplotlib */
     event.preventDefault();
     return false;
-}
+};
 
 mpl.figure.prototype._key_event_extra = function(event, name) {
     // Handle any extra behaviour associated with a key event
-}
+};
 
 mpl.figure.prototype.key_event = function(event, name) {
 
@@ -523,7 +516,7 @@ mpl.figure.prototype.key_event = function(event, name) {
     if (name == 'key_release')
         this._key = null;
 
-    var value = '';
+    let value = '';
     if (event.ctrlKey && event.which != 17)
         value += "ctrl+";
     if (event.altKey && event.which != 18)
@@ -539,7 +532,7 @@ mpl.figure.prototype.key_event = function(event, name) {
     this.send_message(name, {key: value,
         guiEvent: simpleKeys(event)});
     return false;
-}
+};
 
 mpl.figure.prototype.toolbar_button_onclick = function(name) {
     if (name == 'download') {
