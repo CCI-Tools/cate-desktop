60879e51734282f3dc0e029f917281f1686050b8
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2017-10-09 14:33:39 +0200

    switch to 1.0.0-dev.3

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,5 @@
+## 1.0.0-dev.3
+
 ## 1.0.0-dev.2
 
 ### UX Improvements and new Features
--- src/main/main.ts
+++ src/main/main.ts
@@ -34,7 +34,7 @@ const dialog = electron.dialog;
  * @type {string}
  */
 // export const WEBAPI_VERSION_RANGE = ">=0.9.0-dev.5 <=0.9.0-dev.7";
-export const WEBAPI_VERSION_RANGE = "1.0.0-dev.2";
+export const WEBAPI_VERSION_RANGE = "1.0.0-dev.3";
 
 const WEBAPI_INSTALLER_CANCELLED = 1;
 const WEBAPI_INSTALLER_ERROR = 2;
