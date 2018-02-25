74db3c15a73733cf9b57fcc0d0f2980ca7233379
Norman Fomferra <norman.fomferra@gmail.com>
2018-02-25 08:20:12 +0100

    now using electron-update instead

--- src/main/update-frontend.ts
+++ src/main/update-frontend.ts
@@ -1,41 +1,42 @@
-import {app, autoUpdater, dialog} from "electron";
-import * as os from "os";
+import {autoUpdater} from "electron-updater";
+import * as log from "electron-log";
+import {CATE_DESKTOP_PREFIX} from "./main";
 
 
 export function installAutoUpdate() {
 
-    const platform = os.platform();
-    if (platform === "linux") {
-        // Electron auto-update is not available for Linuxes
-        return;
-    }
+    log.transports.file.level = 'info';
+    autoUpdater.logger = log;
 
-    const server = 'https://your-deployment-url.com';
-    const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
-
-    autoUpdater.setFeedURL(feed);
-
-    autoUpdater.on('error', message => {
-        console.error('There was a problem updating the application');
-        console.error(message);
+    autoUpdater.checkForUpdatesAndNotify().then(() => {
+        log.info(CATE_DESKTOP_PREFIX, "Update-checking installed.");
     });
 
-    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-        const dialogOpts = {
-            type: 'info',
-            buttons: ['Restart', 'Later'],
-            title: 'Application Update',
-            message: process.platform === 'win32' ? releaseNotes : releaseName,
-            detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-        };
-
-        dialog.showMessageBox(dialogOpts, (response) => {
-            if (response === 0) {
-                autoUpdater.quitAndInstall();
-            }
-        })
-    });
-
-    // Check for updates every minute
-    setInterval(() => autoUpdater.checkForUpdates(), 60000);
+    //-------------------------------------------------------------------
+    // Auto updates - Option 2 - More control
+    //
+    // For details about these events, see the Wiki:
+    // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
+    //
+    // The app doesn't need to listen to any events except `update-downloaded`
+    //
+    // Uncomment any of the below events to listen for them.  Also,
+    // look in the previous section to see them being used.
+    //-------------------------------------------------------------------
+    // app.on('ready', function()  {
+    //   autoUpdater.checkForUpdates();
+    // });
+    // autoUpdater.on('checking-for-update', () => {
+    // })
+    // autoUpdater.on('update-available', (info) => {
+    // })
+    // autoUpdater.on('update-not-available', (info) => {
+    // })
+    // autoUpdater.on('error', (err) => {
+    // })
+    // autoUpdater.on('download-progress', (progressObj) => {
+    // })
+    // autoUpdater.on('update-downloaded', (info) => {
+    //   autoUpdater.quitAndInstall();
+    // })
 }
