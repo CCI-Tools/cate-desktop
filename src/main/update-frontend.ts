0291ac112cee51c179901ed7b6797dca321fa0d8
Norman Fomferra <norman.fomferra@gmail.com>
2018-02-27 16:43:37 +0100

    testing custom auto-update...

--- src/main/update-frontend.ts
+++ src/main/update-frontend.ts
@@ -1,5 +1,9 @@
-import {autoUpdater, UpdateCheckResult} from "electron-updater";
+import * as electron from "electron";
+import {autoUpdater, UpdateCheckResult, UpdateInfo, CancellationToken} from "electron-updater";
 import * as log from "electron-log";
+
+
+const USER_INTERACTION = true;
 
 
 export function installAutoUpdate() {
@@ -7,40 +11,87 @@ export function installAutoUpdate() {
     autoUpdater.logger = log;
 
     log.info("Installing update-check...");
-    const promise = autoUpdater.checkForUpdatesAndNotify();
-    if (promise) {
-        promise && promise.then((result: UpdateCheckResult) => {
+
+    if (!USER_INTERACTION) {
+        //-------------------------------------------------------------------
+        // Auto updates - Option 1 - Simplest version
+        //
+        // This will immediately download an update, then install when the
+        // app quits.
+        //-------------------------------------------------------------------
+        const promise = autoUpdater.checkForUpdatesAndNotify();
+        if (promise) {
+            promise && promise.then((result: UpdateCheckResult) => {
+                log.info("Update-check result:", result);
+            });
+        } else {
+            log.error("Update-check NOT installed");
+        }
+    } else {
+        //-------------------------------------------------------------------
+        // Auto updates - Option 2 - More control
+        //
+        // For details about these events, see the Wiki:
+        // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
+        //
+        // The app doesn't need to listen to any events except `update-downloaded`
+        //
+        // Uncomment any of the below events to listen for them.  Also,
+        // look in the previous section to see them being used.
+        //-------------------------------------------------------------------
+
+        const cancellationToken: CancellationToken = new CancellationToken();
+        autoUpdater.autoDownload = false;
+        autoUpdater.autoInstallOnAppQuit = false;
+
+        autoUpdater.checkForUpdates().then((result: UpdateCheckResult) => {
             log.info("Update-check result:", result);
         });
-    } else {
-        log.error("Update-check NOT installed");
-    }
 
-    //-------------------------------------------------------------------
-    // Auto updates - Option 2 - More control
-    //
-    // For details about these events, see the Wiki:
-    // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-    //
-    // The app doesn't need to listen to any events except `update-downloaded`
-    //
-    // Uncomment any of the below events to listen for them.  Also,
-    // look in the previous section to see them being used.
-    //-------------------------------------------------------------------
-    // app.on('ready', function()  {
-    //   autoUpdater.checkForUpdates();
-    // });
-    // autoUpdater.on('checking-for-update', () => {
-    // })
-    // autoUpdater.on('update-available', (info) => {
-    // })
-    // autoUpdater.on('update-not-available', (info) => {
-    // })
-    // autoUpdater.on('error', (err) => {
-    // })
-    // autoUpdater.on('download-progress', (progressObj) => {
-    // })
-    // autoUpdater.on('update-downloaded', (info) => {
-    //   autoUpdater.quitAndInstall();
-    // })
+        autoUpdater.on('checking-for-update', () => {
+        });
+
+        autoUpdater.on('update-available', (info) => {
+            let options: electron.MessageBoxOptions = {
+                title: "Cate Desktop",
+                message: `Do you want to download and install it now?`,
+                detail: `A new version of Cate Desktop is available:\nVersion ${info.version} from ${info.releaseDate}`,
+                //checkboxLabel: "Disable automatic updates",
+                //checkboxChecked: false,
+                buttons: ["Yes", "No"],
+            };
+            electron.dialog.showMessageBox(options, (response: number/*, checkboxChecked: boolean*/) => {
+                if (response === 0) {
+                    autoUpdater.downloadUpdate(cancellationToken);
+                }
+            });
+        });
+
+        autoUpdater.on('update-not-available', (info) => {
+        });
+
+        autoUpdater.on('error', (err) => {
+            electron.dialog.showErrorBox("Cate Desktop Update", (err && err.toString()) || "Unknown error.");
+        });
+
+        autoUpdater.on('download-progress', (progressObj) => {
+            log.silly(progressObj);
+        });
+
+        autoUpdater.on('update-downloaded', (info: UpdateInfo) => {
+            let options: electron.MessageBoxOptions = {
+                title: "Cate Desktop Update",
+                message: `Update to version ${info.version} (from ${info.releaseDate})?`,
+                detail: "A new version of Cate Desktop is ready to be installed.",
+                //checkboxLabel: "Disable automatic updates",
+                //checkboxChecked: false,
+                buttons: ["Yes", "No"],
+            };
+            electron.dialog.showMessageBox(options, (response: number/*, checkboxChecked: boolean*/) => {
+                if (response === 0) {
+                    autoUpdater.quitAndInstall();
+                }
+            });
+        });
+    }
 }
