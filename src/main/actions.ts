636ea12f4d2c172db089ef716c75cfddf1d66b2d
Norman Fomferra <norman.fomferra@gmail.com>
2019-12-04 16:29:35 +0100

    Update src/main/actions.ts
    Co-Authored-By: SabineEmbacher <sabine.embacher@brockmann-consult.de>

--- src/main/actions.ts
+++ src/main/actions.ts
@@ -290,6 +290,7 @@ export const actions = {
             const detail = '' +
                            `Program: ${electron.app.getAppPath()}\n` +
                            `Data: ${getAppDataDir()}\n` +
+                           // TODO (SabineEmbacher): find a better solution for context injection
                            `Web API: ${getWebAPIRestUrl(electron.app['_configuration'].webAPIConfig)}\n` +
                            `CLI env: ${getCateDir() ? getCateDir() : '<unknown>'}\n` +
                            `Requires Cate Core ${CATE_WEBAPI_VERSION_RANGE}\n` +
@@ -304,4 +305,3 @@ export const actions = {
         },
     },
 };
-
